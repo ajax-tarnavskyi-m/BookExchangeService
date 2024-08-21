3a768455dbcc7bfbebcad2dcf6f5f7e11f2ceea2
Mikhaylo Tarnavskyi <mikhaylotarnavskyi@Mikhaylos-MacBook-Pro.local>
2024-08-21 15:01:33 +0300

    added detect and improved code quality

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
-    kotlin("jvm") version "1.9.24"
-    kotlin("plugin.spring") version "1.9.24"
+    id("io.gitlab.arturbosch.detekt") version "1.23.6"
+    kotlin("jvm") version "1.9.23"
+    kotlin("plugin.spring") version "1.9.23"
     id("org.springframework.boot") version "3.3.2"
     id("io.spring.dependency-management") version "1.1.6"
 }
