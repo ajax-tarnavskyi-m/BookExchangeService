94bdba0a1a92efb4b13161420c77103a54473ac3
Mykhailo Tarnavskyi <tarnavskyi.m@ajax.systems>
2024-09-12 12:01:00 +0300

    changing branch name

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,12 @@
+import io.github.surpsg.deltacoverage.gradle.DeltaCoverageConfiguration
+
 plugins {
     id("io.gitlab.arturbosch.detekt") version "1.23.6"
     kotlin("jvm") version "1.9.23"
     kotlin("plugin.spring") version "1.9.23"
     id("org.springframework.boot") version "3.3.2"
     id("io.spring.dependency-management") version "1.1.6"
+    id("io.github.surpsg.delta-coverage") version "2.4.0"
 }
 
 group = "pet.project"
@@ -22,8 +25,7 @@ repositories {
 dependencies {
     implementation("org.apache.logging.log4j:log4j-api-kotlin:1.5.0")
     implementation("org.springframework.boot:spring-boot-starter-validation:3.3.2")
-    implementation ("io.github.microutils:kotlin-logging-jvm:2.0.11")
-    implementation ("org.springframework.boot:spring-boot-starter-data-mongodb")
+    implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
@@ -46,4 +48,16 @@ kotlin {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+configure<DeltaCoverageConfiguration> {
+    val targetBranch = project.properties["diffBase"]?.toString() ?: "main"
+    diffSource.byGit {
+        compareWith(targetBranch)
+    }
+    violationRules.failIfCoverageLessThan(0.85)
+}
+
+tasks.check {
+    dependsOn(tasks.deltaCoverage)
 }
