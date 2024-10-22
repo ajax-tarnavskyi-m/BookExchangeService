713360a5928e79b2d02be14224cebb1e5238de95
Mykhailo Tarnavskyi <tarnavskyi.m@ajax.systems>
2024-10-22 16:17:16 +0300

    Creating general build file with general logic

---
+++ build.gradle.kts
@@ -0,0 +1,7 @@
+allprojects {
+    group = "сom.example"
+    version = "0.0.1-SNAPSHOT"
+    repositories {
+        mavenCentral()
+    }
+}
--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -9,17 +9,10 @@ plugins {
     id("io.github.surpsg.delta-coverage") version "2.4.0"
 }
 
-group = "pet.project"
-version = "0.0.1-SNAPSHOT"
-
 java {
     toolchain {
         languageVersion = JavaLanguageVersion.of(17)
     }
-}
-
-repositories {
-    mavenCentral()
 }
 
 dependencies {
